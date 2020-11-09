0440e0f0c65e1e2f7e970142ea6de837f0bf95d5
zqm64 <zqm64@outlook.com>
2020-11-09 10:38:30 +0800

    ulysse: Remove property blacklists

--- omni_ulysse.mk
+++ omni_ulysse.mk
@@ -32,11 +32,6 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 5A / Y1
 PRODUCT_MANUFACTURER := Xiaomi
 
-PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
-    ro.product.device \
-    ro.product.name \
-    ro.build.product
-
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.build.security_patch=2099-12-31 \
 	ro.secure=1 \
